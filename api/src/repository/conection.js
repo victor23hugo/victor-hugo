5201acd7e2e142ca62464eca890a9133cc9052b8
Victor Hugo Martins dos Santos <vh23032005@gmail.com>
2022-05-31 11:46:16 -0300

    oi

---
+++ api/src/repository/conection.js
@@ -0,0 +1,13 @@
+import mysql from 'mysql2/promise'
+
+const com = mysql.createConnection({
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PWD,
+    database: process.env.MYSQL_DB
+
+
+
+})
+
+export { con }
